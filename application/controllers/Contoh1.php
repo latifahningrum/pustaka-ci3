9b60c01c462d4589800a636552638c06442e432b
latifahningrum <latifahningrum11@gmail.com>
2022-03-27 12:40:07 +0700

    menambahkan isi pustaka-booking beserta contoh 1,2,3 dan menghilangkan file index

---
+++ application/controllers/Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller 
+{ 
+    public function index() 
+    {
+        "<h1>Perkenalkan</h1>";
+        "Nama saya Latifah Ningrum
+        Saya tinggal di daerah Cengkareng,
+        olah raga yang saya sukai adalah 
+        Bulutangkis"; 
+    }
+}
